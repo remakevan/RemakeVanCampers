3b1a35e6ddc3aad9bc801f7274dbd8c9cd5f99c6
remakevan <info.remakevan@gmail.com>
2025-03-21 19:22:04 +0100

    cambio de ruta

--- vite.config.ts
+++ vite.config.ts
@@ -6,31 +6,18 @@ import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(__filename);const isProduction = process.env.NODE_ENV === "production";
 
+const basePath = isProduction ? "/assets/" : "/";
 export default defineConfig({
+  base: basePath,
   plugins: [
     react(),
     runtimeErrorOverlay(),
     themePlugin(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
   ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
-    },
-  },
-  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
-    emptyOutDir: true,
-  },
+    outDir: "docs",
+  }
 });
+
