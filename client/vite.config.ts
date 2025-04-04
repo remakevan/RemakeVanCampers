7ecac7b97812d2b0d7cee1b9dbfc2eda29a37eaa
remakevan <info.remakevan@gmail.com>
2025-04-04 18:58:44 +0200

    Deploy listo

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["drizzle-orm/pg-core"]
-    }
-  }
+  base: './',
+  plugins: [react()],
 });
